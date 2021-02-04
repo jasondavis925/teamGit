4337a3fe32a51a47e61b4170821d6f7163b0f05f
bmcdougalle <bmcdougalle@gmail.com>
2021-02-03 22:49:49 -0500

    Added PostService with crud methods, added Post Create in models with properties, added PostController with crud methods, added PostEdit in models"

---
+++ SocialMedia.Models/PostCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class PostCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage ="Please enter at least 2 characters")]
+        [MaxLength(200, ErrorMessage ="There are too many characters in this field")]
+        public string Title { get; set; }
+
+        [MaxLength(10000)]
+        public string  Content { get; set; }
+    }
+}
---
+++ SocialMediaAPI/Controllers/PostController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMediaAPI.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+                return postService;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPostById(id);
+            return Ok();
+        }
+
+        public IHttpActionResult Put(PostEdit post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.UpdatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int Id)
+        {
+            var service = CreatePostService();
+            if (!service.DeletePost(Id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
