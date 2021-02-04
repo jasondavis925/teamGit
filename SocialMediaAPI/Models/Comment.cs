d80136cf2d360e93d3dc41fa8a6100ded59859c3
Luke <lukereibel@gmail.com>
2021-02-04 15:59:53 -0500

    lukey3

--- SocialMediaAPI/Models/Comment.cs
+++ SocialMediaAPI/Models/Comment.cs
@@ -7,5 +7,14 @@ namespace SocialMediaAPI.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public int GuidAuthor { get; set; }
+
+        //public List<Reply> Reply { get; set; }
+
+        //foreign key via original Post
     }
 }
